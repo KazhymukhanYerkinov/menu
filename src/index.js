8d397231650af5f9018208c9544b800f06fda7a8
KazhymukhanYerkinov <61079138+KazhymukhanYerkinov@users.noreply.github.com>
2021-01-10 05:30:15 +0600

    '...'

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter basename = {process.env.PUBLIC_URL}>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
